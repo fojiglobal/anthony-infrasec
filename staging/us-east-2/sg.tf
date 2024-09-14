d1d223aaa8a10cae3f33c9d2be8f20ed61e7c8ea
Anthony <anthonyadamiro@gmail.com>
2024-09-13 20:17:34 -0500

    adding my sg_pub to repo

---
+++ staging/us-east-2/sg.tf
@@ -0,0 +1,30 @@
+resource "aws_security_group" "pub_sg" {
+  # ... other configuration ...
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+    ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "public-sg"
+  }
+}
