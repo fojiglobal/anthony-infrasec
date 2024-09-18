5dfc0013ee45441f17615b34625a20910286c0f3
adamiro <anthonyadamiro@gmail.com>
2024-09-18 18:41:19 -0500

    Updating my staging environment files progrress up to listener class instructions. This will allow me to work on my laptop

--- staging/us-east-2/asg.tf
+++ staging/us-east-2/asg.tf
@@ -1,10 +1,10 @@
 resource "aws_launch_template" "staging_lt" {
-  name   = "${var.staging_env}-lt"
-  image_id      = "ami-085f9c64a9b75eed5"
-  instance_type = "t2.micro"
+  name                                 = "${var.staging_env}-lt"
+  image_id                             = "ami-085f9c64a9b75eed5"
+  instance_type                        = "t2.micro"
   instance_initiated_shutdown_behavior = "terminate"
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
-  user_data = filebase64("web.sh")
+  vpc_security_group_ids               = [aws_security_group.private_sg.id]
+  user_data                            = filebase64("web.sh")
 
   tag_specifications {
     resource_type = "instance"
@@ -12,18 +12,24 @@ resource "aws_launch_template" "staging_lt" {
     tags = {
       Name = "staging-env-lt"
     }
- }
+  }
 }
 
+########################### ASG ###############################
 resource "aws_autoscaling_group" "staging_asg" {
-  name               = "${var.staging_env}-asg"
+  name                = "${var.staging_env}-asg"
   vpc_zone_identifier = [aws_subnet.staging_pri_1.id, aws_subnet.staging_pri_2.id]
-  desired_capacity   = 2
-  max_size           = 2
-  min_size           = 1
+  desired_capacity    = 2
+  max_size            = 2
+  min_size            = 1
 
   launch_template {
     id      = aws_launch_template.staging_lt.id
     version = "$Latest"
+  }
+  tag {
+    key                 = "name"
+    value               = "${var.staging_env}-web"
+    propagate_at_launch = true
   }
 }
--- staging/us-east-2/variable.tf
+++ staging/us-east-2/variable.tf
@@ -5,7 +5,7 @@ variable "staging_vpc_cidr" {
 
 variable "qa_vpc_cidr" {
   type    = string
-  default = "10.30.0.0/16"
+  default = "10.40.0.0/16"
 }
 
 variable "disable_sub" {
@@ -26,4 +26,29 @@ variable "use2b" {
 variable "staging_env" {
   type    = string
   default = "staging"
+}
+
+variable "alb_port_http" {
+  type    = string
+  default = "80"
+}
+
+variable "alb_port_https" {
+  type    = string
+  default = "443"
+}
+
+variable "alb_proto_http" {
+  type    = string
+  default = "HTTP"
+}
+
+variable "alb_proto_https" {
+  type    = string
+  default = "HTTPS"
+}
+
+variable "alb_sss_profile" {
+  type    = string
+  default = "ELBSecurityPolicy-2016-08"
 }
