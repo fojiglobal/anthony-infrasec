b73525490d8248467a94960bfa48638da2533b43
adamiro <anthonyadamiro@gmail.com>
2024-10-16 15:22:07 -0500

    Snyk vulnerability remidiation for the vuln.tf file

--- vuln/vuln.tf
+++ vuln/vuln.tf
@@ -5,16 +5,16 @@ resource "aws_s3_bucket" "example" {
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_ebs_volume" "example" {
   availability_zone = "us-west-2a"
   size              = 40
-
+  encrypted         = true
   tags = {
     Name = "HelloWorld"
   }
@@ -27,7 +27,7 @@ resource "aws_lb" "test" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.lb_sg.id]
   subnets            = [for subnet in aws_subnet.public : subnet.id]
-
+  drop_invalid_header_fields = true
   enable_deletion_protection = false
 
   tags = {
@@ -38,7 +38,7 @@ resource "aws_lb" "test" {
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.front_end.arn
   port              = "80"
-  protocol          = "HTTP"
+  protocol          = "HTTPS"
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.front_end.arn
