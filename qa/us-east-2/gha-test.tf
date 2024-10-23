12a46538bcf80063ffe554f92dc5b503524f5d9b
adamiro <anthonyadamiro@gmail.com>
2024-10-23 12:07:40 -0500

    Added Working Directories and more TF Files

--- qa/us-east-2/gha-test.tf
+++ qa/us-east-2/gha-test.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "qa" {
-  cidr_block = "10.11.0.0/16"  
+  cidr_block = "10.11.0.0/16"
   tags = {
-    Name = "qa_vpc"
+    Name        = "qa_vpc"
     Environment = "qa"
   }
 }
