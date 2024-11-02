12131694b117551caab9964788f9203f052fbb5e
adamiro <anthonyadamiro@gmail.com>
2024-11-01 23:29:05 -0500

    Remidiated S3 Bucket Misconfiguration

--- qa/us-east-2/gha-test.tf
+++ qa/us-east-2/gha-test.tf
@@ -13,9 +13,9 @@ resource "aws_s3_bucket" "example" {
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.example.id
-  
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
